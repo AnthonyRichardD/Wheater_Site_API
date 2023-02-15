976643e9af3147bb628bd19c2c7e3f52ea1478c3
Anthony Richard <anthonyricharddasilvamartins2@gmail.com>
2023-02-15 14:24:22 -0300

    Script consumindo api de clima e modificando os elementos

---
+++ script/script.js
@@ -0,0 +1,39 @@
+const form = document.querySelector(".form");
+const input = document.querySelector("#input_pesquisa");
+const pais = document.querySelector(".pais");
+const estado= document.querySelector(".estado");
+const cidade = document.querySelector(".cidade");
+const icon = document.querySelector(".clima-icon");
+const temperatura = document.querySelector(".temperatura");
+const condicao = document.querySelector(".condicao");
+const apikey = process.env.SECRET_KEY;
+
+let pesquisa = "";
+async function cidadeFetch(nome){
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${nome.toLowerCase()}&aqi=no&lang=pt`)
+    if(response.status === 200){
+        const cidade = await response.json()
+        pesquisa = await cidade
+        alterarPesquisa()
+    }
+    else{
+        console.log("não encontrado")
+    }
+
+};
+cidadeFetch("recife");
+function alterarPesquisa(){
+    estado.innerHTML = pesquisa.location.region
+    cidade.innerHTML = pesquisa.location.name
+    pais.innerHTML = pesquisa.location.country
+    icon.src = pesquisa.current.condition.icon
+    temperatura.innerHTML = pesquisa.current.temp_c + " °C"
+    input.value = ""
+    console.log(pesquisa.current.conditions)
+    condicao.innerHTML = pesquisa.current.condition.text
+};
+
+form.addEventListener('submit',(event) =>{
+    event.preventDefault();
+    cidadeFetch(input.value)
+});
